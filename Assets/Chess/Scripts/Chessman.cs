8ad957ef7f080eedf9abbb771771fa5021437adf
Kpenisto <kylepeniston@gmail.com>
2018-03-01 14:18:40 -0600

    Chess game using Unity Framework

---
+++ Assets/Chess/Scripts/Chessman.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Chessman : MonoBehaviour
+{
+    public int CurrentX { set; get; }
+    public int CurrentY { set; get; }
+    public bool isWhite;
+
+    //Positioning of Pieces
+    public void SetPosition(int x, int y)
+    {
+        CurrentX = x;
+        CurrentY = y;
+    }
+
+    //Boundaries for pieces and board
+    public virtual bool[,] PossibleMove()
+    {
+        return new bool[8,8];
+    }
+	
+}
